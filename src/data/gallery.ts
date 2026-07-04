eff22c7d3937479d1752607f4024cf63c4a5d569
FatDunDun <micylt710@gmail.com>
2026-07-04 16:13:49 +0800

    Sync site updates: write 1 content files, upload 2 media files

--- src/data/gallery.ts
+++ src/data/gallery.ts
@@ -1,5 +1,11 @@
 export const galleryItems = [
   {
+    "title": "春日",
+    "description": "樱花树下",
+    "image": "https://pub-f391fe6f8e924935bcda42a0b88e5143.r2.dev/personal-photo/1783152725147-img-3036-83d6ba9b.jpg",
+    "height": "medium"
+  },
+  {
     "title": "婚纱剪影",
     "description": "一只在草坪上拍婚纱照的猪",
     "image": "https://pub-f391fe6f8e924935bcda42a0b88e5143.r2.dev/personal-photo/1782832614633-bg-married-03-e1cae563.jpg",
